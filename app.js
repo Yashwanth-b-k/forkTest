9883560622ded142bc9289e5ceda70c6a6796a70
Yashwanth B K <132933255+Yashwanth-b-k@users.noreply.github.com>
2025-08-12 15:47:12 +0530

    Update app.js
    this fixes #7

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function greet() {
   console.log("Hello, Good Evening!");
 }
-function farewell() {
-  console.log("have a nice day!");
+function farewell(name) {
+  console.log("have a nice day!" + name);
 }
 console.log("hi from duplicate dev, i have changed the log");
