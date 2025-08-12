c7df220cd93beddb9b1bbcc77adec61efe16e3e3
Yashwanth B K <132933255+Yashwanth-b-k@users.noreply.github.com>
2025-08-12 15:46:16 +0530

    Update app.js
    created farewell as per #7

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 function greet() {
   console.log("Hello, Good Evening!");
 }
+function farewell() {
+  console.log("have a nice day!");
+}
 console.log("hi from duplicate dev, i have changed the log");
